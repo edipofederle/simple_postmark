de9ae62f546f9891180d90749bae9989997b5d8d
Édipo Féderle <edipofederle@gmail.com>
2022-10-08 18:25:32 -0300

    Use https

--- lib/simple_postmark/delivery_method.rb
+++ lib/simple_postmark/delivery_method.rb
@@ -2,14 +2,14 @@ module Mail
   class SimplePostmark < Struct.new(:settings)
     include HTTParty
 
-    base_uri 'http://api.postmarkapp.com'
+    base_uri 'https://api.postmarkapp.com'
     headers 'Accept' => 'application/json', 'ContentType' => 'application/json'
     delegate :post, :headers, to: self
 
     def deliver!(mail)
       headers = self.headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s)
       body = mail.to_postmark.to_json
-      
+
       response = post('/email', headers: headers, body: body)
       raise ::SimplePostmark::APIError.new(response) unless response.success?
       response
